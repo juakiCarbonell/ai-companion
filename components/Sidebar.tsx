15d2ca37fbb896812eb4fb30384d33c14023b027
Joaquin Carbonell <joaquin.carbonell@babelgroup.com>
2024-10-28 09:24:41 +0100

    search bar added

---
+++ components/Sidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import {Home, Plus, Settings} from "lucide-react";
+import {usePathname, useRouter} from "next/navigation";
+
+import {cn} from "@/lib/utils";
+
+const routes = [
+  {
+    label: "Home",
+    href: "/",
+    icon: Home,
+    pro: false,
+  },
+  {
+    label: "Create",
+    href: "/companion/new",
+    icon: Plus,
+    pro: true,
+  },
+  {
+    label: "Settings",
+    href: "/settings",
+    icon: Settings,
+    pro: false,
+  },
+];
+
+export const Sidebar = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+
+  const onNavigate = (url: string) => {
+    //TODO: check if pro
+    router.push(url);
+  };
+
+  return (
+    <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
+      <div className="p-3 flex-1 flex justify-center">
+        <div className="space-y-2">
+          {routes.map((route) => (
+            <div
+              key={route.href}
+              onClick={() => onNavigate(route.href)}
+              className={cn(
+                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathName === route.href && "bg-primary/10 text-primary"
+              )}
+            >
+              <div className="flex flex-col gap-y-2 items-center flex-1">
+                <route.icon className="h-5 w-5" />
+                {route.label}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
