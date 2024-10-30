2fc46c7a7a4a4fd8ee6cf5410ce5010f84d41c07
Joaquin Carbonell <joaquin.carbonell@babelgroup.com>
2024-10-30 11:08:03 +0100

    add db and

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,15 @@
-import { SearchInput } from "@/components/search-input";
+import { Categories } from "@/components/categories";
+import {SearchInput} from "@/components/search-input";
+import prismaDb from "@/lib/prismadb";
 import React from "react";
 
-const RootPage = () => {
+const RootPage = async () => {
+  const categories = await prismaDb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
